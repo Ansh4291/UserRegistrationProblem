2bb1a3a1e8911aa8e76922f61fa577ca1148671d
AnshulRathore <anshulrathore1995@gmail.com>
2022-05-10 16:38:36 +0530

    Added:Welcome messgage added

---
+++ src/main/java/com/bridgelabz_userRegistration/UserRegistrationMain.java
@@ -0,0 +1,7 @@
+package com.bridgelabz_userRegistration;
+
+public class UserRegistrationMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the user registration problem:-) ");
+    }
+}
